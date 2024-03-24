43fda0ace676cc0294c28e552eca5e1fbc100f87
Ojima <akbenngold@gmail.com>
2024-03-24 16:19:41 +0100

    svg filepath change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ function Header() {
     <div className="header">
       <img
         className="header__logo"
-        src="my-portfolio-website/src/assets/ojima-high-resolution-logo-white-transparent.png"
+        src="public/ojima-high-resolution-logo-white-transparent.png"
         alt="logo"
       />
       <img
         className="header__hamburger"
-        src="my-portfolio-website/src/assets/hamburger.svg"
+        src="public/hamburger.svg"
         alt="hamburger icon"
       />
     </div>
