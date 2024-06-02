7f44f2936ecab7f9d6017ac0d5b2d72523736ac7
akbenngold <akbenngold@gmail.com>
2024-06-02 07:55:15 +0100

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,30 +4,32 @@ import { FaGithub, FaHashnode, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
-    <footer className="footer p-10 bg-neutral-800 text-neutral-content flex justify-between items-center gap-12 section-container">
-      <aside className="text-4xl md:text-7xl">OJ_</aside>
-      <nav className="flex flex-col gap-4 items-center">
-        <h6 className="footer-title">Socials</h6>
-        <div className="flex gap-4">
-          <a href="https://x.com/akbenngold" target="_blank">
-            <FaXTwitter size="1.5rem" />
-          </a>
-          <a href="https://github.com/akbenngold" target="_blank">
-            <FaGithub size="1.5rem" />
-          </a>
-          <a href="https://hashnode.com/akbenngold" target="_blank">
-            <FaHashnode size="1.5rem" />
-          </a>
-          <a href="https://linkedin.com/akbenngold" target="_blank">
-            <FaLinkedin size="1.5rem" />
-          </a>
+    <footer className="footer p-10 bg-neutral-800 text-neutral-content flex flex-col  gap-12 section-container items-center">
+      <div className="flex justify-between items-center w-[100%]">
+        <aside className="text-4xl md:text-7xl">OJ_</aside>
+        <nav className="flex flex-col gap-4 items-center">
+          <h6 className="footer-title">Socials</h6>
+          <div className="flex gap-4">
+            <a href="https://x.com/akbenngold" target="_blank">
+              <FaXTwitter size="1.5rem" />
+            </a>
+            <a href="https://github.com/akbenngold" target="_blank">
+              <FaGithub size="1.5rem" />
+            </a>
+            <a href="https://hashnode.com/akbenngold" target="_blank">
+              <FaHashnode size="1.5rem" />
+            </a>
+            <a href="https://linkedin.com/akbenngold" target="_blank">
+              <FaLinkedin size="1.5rem" />
+            </a>
 
-          <a href="mailto:akbenngold@gmail.com" target="_blank">
-            <AiOutlineMail size="1.5rem" />
-          </a>
-        </div>
-        <p> @akbenngold</p>
-      </nav>
+            <a href="mailto:akbenngold@gmail.com" target="_blank">
+              <AiOutlineMail size="1.5rem" />
+            </a>
+          </div>
+        </nav>
+      </div>
+      <p className="">© Ojima || 2024</p>
     </footer>
   );
 }
