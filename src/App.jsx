84d58f231803f17b081434cd38dd44bac3d04cb2
akbenngold <akbenngold@gmail.com>
2024-08-22 00:55:08 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Hero from "./components/Hero";
@@ -10,21 +14,50 @@ import ContactMe from "./components/ContactMe";
 import ProgressBar from "./components/ProgressBar";
 import AboutMe from "./components/AboutMe";
 
+function Section({ children }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section ref={ref}>
+      <span
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  );
+}
+
 function App() {
   return (
     <div className=" bg-[#f0f0ed] overflow-hidden  relative">
       <Navbar />
       <ProgressBar />
-      <Hero />
+      <Section>
+        <Hero />
+      </Section>
 
       <Skills />
-      <Services />
+      <Section>
+        <Services />
+      </Section>
       <hr />
-      <Portfolio />
+      <Section>
+        <Portfolio />
+      </Section>
       <hr />
-      <AboutMe />
+      <Section>
+        <AboutMe />
+      </Section>
       <hr />
-      <ContactMe />
+      <Section>
+        <ContactMe />
+      </Section>
       <Footer />
     </div>
   );
